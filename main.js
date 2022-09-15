acc17dad7d6e0ff750eccf94bf0a322e317ef544
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-15 22:52:52 +0000

    trying shortened variables for time

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ setInterval(updateTime, 1000);
 
 function updateTime() {
     let clockTime = new Date();
-    let timeHour = clockTime.getHours();
+    /*let timeHour = clockTime.getHours();
     let timeMinutes = clockTime.getMinutes();
-    let timeSeconds = clockTime.getSeconds();
-    let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;
-    document.getElementById('clock').innerHTML = currentTime;
+    let timeSeconds = clockTime.getSeconds();*/
+    //let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;
+    let twelveHour = clockTime.toLocaleTimeString('en-US');
+    document.getElementById('clock').innerHTML = twelveHour;
 }
