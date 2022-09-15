0e72f4ac8c8462020de9dee1187eac367f5f6c9d
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-15 22:43:12 +0000

    functioning counting clock

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 
-//let clockInterface = document.getElementById('clock');
-
-let clockTime = new Date();
-let timeHour = clockTime.getHours();
-let timeMinutes = clockTime.getMinutes();
-let timeSeconds = clockTime.getSeconds();
-let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;
-
-//document.getElementById('clock').innerHTML = currentTime;
-
-/*setInterval(updateTime, 1000);
+setInterval(updateTime, 1000);
 
 function updateTime() {
-    document.getElementById('clock').innerHTML = currentTime;
-}   */
-const runningTime = setInterval(updateClock, 1000, currentTime);
-function updateClock(currentTime) {
+    let clockTime = new Date();
+    let timeHour = clockTime.getHours();
+    let timeMinutes = clockTime.getMinutes();
+    let timeSeconds = clockTime.getSeconds();
+    let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;
     document.getElementById('clock').innerHTML = currentTime;
 }
