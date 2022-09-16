4866cefa2dce484ab8938a4ccb47a3bb09dd9bff
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-16 01:55:30 +0000

    stopping point

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ function updateTime() {
     let clockTime = new Date();
     /*let timeHour = clockTime.getHours();
     let timeMinutes = clockTime.getMinutes();
-    let timeSeconds = clockTime.getSeconds();*/
-    //let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;*/
+    let timeSeconds = clockTime.getSeconds();
+    let currentTime = timeHour + ":" + timeMinutes + ":" + timeSeconds;*/
     let currentTime = clockTime.toLocaleTimeString('en-US');
     document.getElementById('clock').innerHTML = currentTime;
 }
 const alarmTime = document.getElementById("alarm");
+const timeValue = document.getElementById("value");
+// let submitBtn = document.getElementById("submit");
+// let alarmSet
 
-alarmTime.addEventListener("submit", () => {
-    let setAlarm = alarmTime.value;
-})
+alarmTime.addEventListener('keydown', (e) => {
+    timeValue.innerText = alarmTime.value;
+}, false);
+
+/*function storeAlarm() {
+    console.log(alarmTime);
+};*/
